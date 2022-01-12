9c3a4740502b84293208c2b3bf81e6f5d940de30
gizemdemirtaass <79507052+gizemdemirtaass@users.noreply.github.com>
2022-01-12 21:47:42 +0300

    Add files via upload

---
+++ ScannerProject/src/com/company/Solution.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main (String[] arg){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
